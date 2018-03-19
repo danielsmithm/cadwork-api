be18ee1b53900bdec477c3c31e1260f0bc42ce2d
I-am-Miguel <miguelwelligton@gmail.com>
2018-03-19 17:26:16 -0300

    Visualização por id, listagem geral e adição de serie/etapaEnsino

---
+++ src/main/java/br/com/imd/cadeduc/ensino/resources/EtapaEnsinoResources.java
@@ -0,0 +1,38 @@
+package br.com.imd.cadeduc.ensino.resources;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.imd.cadeduc.ensino.dao.EtapaEnsinoDAO;
+import br.com.imd.cadeduc.ensino.domain.EtapaEnsino;
+
+@RestController
+@RequestMapping(value = "/etapasEnsino")
+public class EtapaEnsinoResources {
+
+	@Autowired
+	EtapaEnsinoDAO etapaEnsinoDao;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<EtapaEnsino> listar() {
+		return etapaEnsinoDao.findAll();
+
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public void salvar(@RequestBody EtapaEnsino etapaEnsino) {
+		etapaEnsinoDao.save(etapaEnsino);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Optional<EtapaEnsino> buscar(@PathVariable("id") Long id) {
+		return etapaEnsinoDao.findById(id);
+	}
+}
