9396fbbf59509373235859234b02ee5699c28e3e
Igor Barbosa Nogueira <igornogueir@gmail.com>
2018-04-25 05:04:16 -0300

    Mostrando nome da escola

--- src/main/java/br/com/imd/cadeduc/escola/resources/EscolaReportController.java
+++ src/main/java/br/com/imd/cadeduc/escola/resources/EscolaReportController.java
@@ -5,30 +5,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.imd.cadeduc.core.service.exception.GenericServiceException;
 import br.com.imd.cadeduc.escola.service.EscolaService;
 
 @Controller
 @RequestMapping("/reports")
-public class EscolaReportResources {
+public class EscolaReportController {
 	
 	@Autowired
 	EscolaService escolaService;
 	
-	public EscolaReportResources(EscolaService escolaService) {
+	public EscolaReportController(EscolaService escolaService) {
 		this.escolaService = escolaService;
 	}
 	
 	
 	@GetMapping("/html")
-	public String html(Model model) {
-		try {
-			model.addAttribute("escolas", this.escolaService.listar());
-		} catch (GenericServiceException e) {
-			e.printStackTrace();
+	public String html(@RequestParam(required = false, value = "escola") Long id, Model model) throws GenericServiceException {
+		
+		model.addAttribute("escolas", this.escolaService.listar());
+		
+		if (id == null) {
+			return "report/index";
 		}
+		
+		model.addAttribute("atribute", this.escolaService.buscar(id).get());
+		
 		return "report/index";
+		
 	}
 	
 }
